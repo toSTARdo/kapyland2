e2a4fb4efa9dd6fdb35fc4261d04c789c94f85d8
TerryForgetfull <160047138+toSTARdo@users.noreply.github.com>
2026-02-07 23:17:47 +0200

    fix: mistakenly used dispatcher instead of router in non-main script

--- core/life_subcore.py
+++ core/life_subcore.py
@@ -3,14 +3,14 @@ from aiogram.filters import Command
 
 router = Router()
 
-@dp.message(Command("feed"))
+@router.message(Command("feed"))
 async def cmd_feed(message: types.Message):
     await message.answer("Капібара поїла та набрала +5кг!")
 
-@dp.message(Command("wash"))
+@router.message(Command("wash"))
 async def cmd_wash(message: types.Message):
     await message.answer("Капібара скупалася та позбулася бліх!")
 
-@dp.message(Command("sleep"))
+@router.message(Command("sleep"))
 async def cmd_sleep(message: types.Message):
     await message.answer("Капібара гарненько відіспалася і готова покоряти моря!")
